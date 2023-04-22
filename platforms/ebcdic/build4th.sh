d3d7d0d6c8b244935d303b9fe14479d297397c3b
Aigor44 <aigor@qemu-debian-7-wheezy.hsh>
2023-04-22 22:06:36 +0200

    Added EBCDIC

---
+++ platforms/ebcdic/build4th.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ 
+# CC="gcc -m31"
+  CC="gcc -m32"
+
+# CFLAGS="-g -O0 -I. -DPF_SUPPORT_FP -DPF_STATIC_DIC -DEBCDIC"
+  CFLAGS="-g -O0 -I. -DPF_SUPPORT_FP -DPF_STATIC_DIC"
+
+${CC} -c ${CFLAGS} ../../csrc/pf_cglue.c
+${CC} -c ${CFLAGS} ../../csrc/pf_clib.c
+
+${CC} -c ${CFLAGS} ../../csrc/pf_core.c
+${CC} -c ${CFLAGS} ../../csrc/pf_inner.c
+${CC} -c ${CFLAGS} ../../csrc/pf_io.c
+${CC} -c ${CFLAGS} ../../csrc/pf_io_none.c
+${CC} -c ${CFLAGS} ../../csrc/ebcdic/pf_io_ebcdic.c
+${CC} -c ${CFLAGS} ../../csrc/stdio/pf_fileio_stdio.c
+${CC} -c ${CFLAGS} ../../csrc/pf_main.c
+${CC} -c ${CFLAGS} ../../csrc/pf_mem.c
+${CC} -c ${CFLAGS} ../../csrc/pf_save.c
+${CC} -c ${CFLAGS} ../../csrc/pf_text.c
+${CC} -c ${CFLAGS} ../../csrc/pf_words.c
+${CC} -c ${CFLAGS} ../../csrc/pfcompil.c
+${CC} -c ${CFLAGS} ../../csrc/pfcustom.c
+ 
+${CC} -o pforth_standalone *.o -lm
