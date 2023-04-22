d0355bacfd10527721fddf45266773ac687c62c1
Aigor44 <117379101+Aigor44@users.noreply.github.com>
2023-04-23 00:56:45 +0200

    Update build4th.sh

--- platforms/ebcdic/build4th.sh
+++ platforms/ebcdic/build4th.sh
@@ -29,3 +29,42 @@ ${CC} -c ${CFLAGS} ../../csrc/pfcustom.c
 ${CC} -o pforth_standalone *.o -lm
 
 rm -f *.o
+
+rm -f pforth.vmarc
+
+vma -at pforth.vmarc \
+  ../../csrc/pf_cglue.c \
+  ../../csrc/pf_clib.c \
+  ../../csrc/pf_core.c \
+  ../../csrc/pf_inner.c \
+  ../../csrc/pf_io.c \
+  ../../csrc/pf_io_none.c,pfionone.c\
+  ../../csrc/pf_main.c \
+  ../../csrc/pf_mem.c \
+  ../../csrc/pf_save.c \
+  ../../csrc/pf_text.c \
+  ../../csrc/pf_words.c \
+  ../../csrc/pfcompil.c \
+  ../../csrc/pfcustom.c \
+  ../../csrc/pf_all.h \
+  ../../csrc/pf_cglue.h \
+  ../../csrc/pf_clib.h \
+  ../../csrc/pf_core.h \
+  ../../csrc/pf_float.h \
+  ../../csrc/pf_guts.h \
+  ../../csrc/pf_host.h \
+  ../../csrc/pf_inc1.h \
+  ../../csrc/pf_io.h \
+  ../../csrc/pf_mem.h \
+  ../../csrc/pf_save.h \
+  ../../csrc/pf_text.h \
+  ../../csrc/pf_types.h \
+  ../../csrc/pf_win32.h \
+  ../../csrc/pf_words.h \
+  ../../csrc/pfcompfp.h \
+  ../../csrc/pfcompil.h \
+  ../../csrc/pfinnrfp.h \
+  ../../csrc/pforth.h \
+  pforth.parm \
+  build4th.exec
+
