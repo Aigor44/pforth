12619c3cafb9462f4d8fa6e2b920542bc7553aa6
Aigor44 <aigor44@proton.me>
2023-04-23 22:49:20 +0200

    Added define for identifiers with max lenght 8 for __GCCCMS__

--- csrc/pf_all.h
+++ csrc/pf_all.h
@@ -1,8 +1,8 @@
 /* @(#) pf_all.h 98/01/26 1.2 */
-
+ 
 #ifndef _pf_all_h
 #define _pf_all_h
-
+ 
 /***************************************************************
 ** Include all files needed for PForth
 **
@@ -24,7 +24,7 @@
 ** 940521 PLB Creation.
 **
 ***************************************************************/
-
+ 
 #ifdef PF_EMBEDDED
     #define PF_NO_INIT
     #define PF_NO_STDIO
@@ -32,57 +32,63 @@
     #define PF_NO_CLIB
     #define PF_NO_FILEIO
 #endif
-
+ 
 /* I don't see any way to pass compiler flags to the Mac Code Warrior compiler! */
 #ifdef __MWERKS__
     #define PF_SUPPORT_FP    (1)
 #endif
-
+ 
 #ifdef WIN32
     #define PF_USER_INC2     "pf_win32.h"
 #endif
-
+ 
 #ifdef __GCCCMS__
-    #define pfCharToUpper              pfC2U
-    #define pfCharToLower              pfC2T
-    #define gCurrentTask               gCTask
-    #define gCurrentDictionary         gCDic
-    #define gVarTraceLevel             gVTL
-    #define gVarTraceStack             gVTS
-    #define gVarTraceFlags             gVTF
-    #define pfCreateTask               pfCTask
-    #define pfCreateDictionary         pfCreDic
-    #define pfDeleteDictionary         pfDelDic
-    #define pfDeleteTask               pfDelTask
-    #define ffCompareTextCaseN         ffCmpTextCaseN
-    #define ffCompare                  ffCmp
     #define CreateDicEntry             CrDicEntry
     #define CreateDicEntryC            cCrDicEntry
-    #define ffCreateSecondaryHeader    ffCrSecondaryHeader
-    #define ffStringColon              ffStringColon
-    #define ffStringCreate             ffStrCreate
-    #define ffConvertStreamToSourceID  ff1ConvertStreamToSourceID
-    #define ffConvertSourceIDToStream  ff2ConvertSourceIDToStream
+    #define ReadCellBigEndian          Read1CellBigEndian
+    #define ReadFloatBigEndian         Read1FloatBigEndian
+    #define ReadFloatLittleEndian      Read2FloatLittleEndian
     #define ReadLongBigEndian          Read1LongBigEndian
     #define ReadLongLittleEndian       Read2LongLittleEndian
     #define ReadShortBigEndian         Read1ShortBigEndian
     #define ReadShortLittleEndian      Read2ShortLittleEndian
+    #define WriteCellBigEndian         Write1CellBigEndian
     #define WriteFloatBigEndian        Write1FloatBigEndian
     #define WriteFloatLittleEndian     Write2FloatLittleEndian
-    #define ReadFloatBigEndian         Read1FloatBigEndian
-    #define ReadFloatLittleEndian      Read2FloatLittleEndian
     #define WriteLongBigEndian         Write1LongBigEndian
     #define WriteLongLittleEndian      Write2LongLittleEndian
     #define WriteShortBigEndian        Write1ShortBigEndian
     #define WriteShortLittleEndian     Write2ShortLittleEndian
-#endif    
 
+    #define ffCompare                  ffCmp
+    #define ffCompareTextCaseN         ffCmpTextCaseN
+    #define ffConvertSourceIDToStream  ff2ConvertSourceIDToStream
+    #define ffConvertStreamToSourceID  ff1ConvertStreamToSourceID
+    #define ffCreateSecondaryHeader    ffCrSecondaryHeader
+    #define ffStringColon              ffStringColon
+    #define ffStringCreate             ffStrCreate
+    #define gCurrentDictionary         gCDic
+    #define gCurrentTask               gCTask
+    #define gIncludeIndex              gIncIndex
+    #define gIncludeStack              gIncStack
+    #define gVarTraceFlags             gVTF
+    #define gVarTraceLevel             gVTL
+    #define gVarTraceStack             gVTS
+    #define pfCharToLower              pfC2T
+    #define pfCharToUpper              pfC2U
+    #define pfCreateDictionary         pfCreDic
+    #define pfCreateTask               pfCTask
+    #define pfDeleteDictionary         pfDelDic
+    #define pfDeleteTask               pfDelTask
+    #define pfReportError              pfRepError
+#endif
+ 
 #if defined(PF_USER_INC1)
     #include PF_USER_INC1
 #else
     #include "pf_inc1.h"
 #endif
-
+ 
 #include "pforth.h"
 #include "pf_types.h"
 #include "pf_io.h"
@@ -95,11 +101,11 @@
 #include "pf_mem.h"
 #include "pf_cglue.h"
 #include "pf_core.h"
-
+ 
 #ifdef PF_USER_INC2
 /* This could be used to undef and redefine macros. */
     #include PF_USER_INC2
 #endif
-
+ 
 #endif /* _pf_all_h */
-
+ 
